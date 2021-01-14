9781e37d093752df7df2fbcad23260ae5ca647dd
StevenDRiggs <sfriggs21@gmail.com>
2021-01-14 07:49:17 -0700

    initial commit

---
+++ env_setup.sh
@@ -0,0 +1,58 @@
+#! bash
+
+
+sudo apt upgrade -y && sudo apt update -y
+sudo apt upgrade -y && sudo apt update -y
+sudo apt install curl -y
+
+read -p 'Email?: ' email
+read -p 'GitHub username?: ' gh_username
+git config --global color.ui true
+git config --global user.name $gh_username
+git config --global user.email $email
+
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >> ~/.bash_profile
+echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >> ~/.profile
+source ~/.bash_profile
+source ~/.profile
+sudo apt install build-essential -y
+brew install gcc
+
+sudo apt install zlib1g-dev libssl-dev libreadline-dev libyaml-dev libsqlite3-dev libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev libpq-dev libgdbm-dev libncurses5-dev automake libtool bison gnupg postgresql postgresql-contrib -y
+# pg_ctlcluster 12 main start
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.1/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+source ~/.bashrc
+nvm install node
+npm install -g npm
+
+brew install rbenv
+rbenv init
+echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+source ~/.bash_profile
+curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-doctor | bash
+rbenv install 2.5.7
+rbenv global 2.5.7
+
+gem install bundler rails
+
+brew install python3
+pip3 install virtualenv
+
+cd ~
+git clone git@github.com/$gh_username/MyConfig.git
+cat MyConfig/.bash_profile >> .bash_profile
+source .bash_profile
+mv MyConfig/.vimrc .
+
+sudo apt install vim -y
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim -c ':PluginInstall' -c ':q' -c ':q' .vimrc
+vim -c ':%s/"//' -c ':w' -c ':source %' -c ':PluginInstall' -c ':q' -c ':q' .vimrc
+vim -c ':%s/"//' -c ':w' -c ':q' .vimrc
+
+rm -rf MyConfig/
