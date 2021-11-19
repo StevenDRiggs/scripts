d05f18718436466be3cb340774d0609d6f79a670
Steven Riggs <sfriggs21@gmail.com>
2021-11-19 14:02:51 -0700

    Update tmux_setup.sh

--- tmux_setup.sh
+++ tmux_setup.sh
@@ -1,43 +1,97 @@
 #! /data/data/com.termux/files/usr/bin/bash
 
 
-rm -rfv ~/MyConfig ~/.bashrc ~/.bash_profile ~/.profile ~/.vim*
-touch ~/.bash_profile
+cd ~
 
-pkg upgrade -y && pkg update -y
-pkg install curl git libyaml libxml2 libffi automake libtool bison gnupg postgresql man openssh nodejs clang -y
+rm -rfv ~/.bashrc ~/.bash_profile ~/.profile ~/.vim*
 
-npm install -g npm
-
-export PATH="$HOME/.rbenv/bin:$PATH"
-curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-installer | bash
-mkdir -p "$(rbenv root)"/plugins
-rbenv init
-curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-doctor | bash
-
-read -p 'GitHub username?: ' gh_username < /dev/tty
-read -p 'Email?: ' email < /dev/tty
+read -p 'git username?: ' git_username
+read -p 'git email?: ' git_email
 git config --global color.ui true
-git config --global user.name $gh_username
-git config --global user.email $email
+git config --global user.name $git_username
+git config --global user.email $git_email
 git config --global submodule.recurse true
-git clone https://github.com/$gh_username/MyConfig.git ~/MyConfig
-cat ~/MyConfig/tmux_bash >> ~/.bash_profile
 
-cd
-source ~/.bash_profile
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$($HOME/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> $HOME/.profile
+eval "$($HOME/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
-rbenv install -v 2.5.7
-rbenv install -v 3.0.0
-rbenv global 3.0.0
+pkg install -y build-essential
 
-gem install bundler rails
+pkg install -y automake bison ca-certificates clang gifsicle gnupg jpegoptim libffi libllvm libtool libxml2 libyaml make man nodejs openssh optipng ruby tree yarn
+# pkg install postgresql
 
-pkg install python -y
-pip install --upgrade pip
-pip install virtualenv
+brew install postgresql
 
-pkg install vim -y
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+nvm install node
+npm install -g npm
+npm install -g yarn
+yarn global add typescript @types/jest
+
+brew install rbenv -v
+rbenv init
+echo '
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
+export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> .bash_profile
+curl -fsSL https://github.com/rbenv/rbenv-installer/raw/main/bin/rbenv-doctor | bash
+
+cat ~/.bash_profile >> ~/.bashrc
+cat ~/.profile >> ~/.bashrc
+
+echo "
+# If not running interactively, don't do anything
+case \$- in
+    *i*) ;;
+      *) return;;
+esac" >> ~/.bashrc
+
+cp ~/.bashrc ~/.bash_profile
+cp ~/.bashrc ~/.profile
+
+echo "
+echo '.bashrc called'
+" >> ~/.bashrc
+
+echo "
+echo '.bash_profile called'
+" >> ~/.bash_profile
+
+echo "
+echo '.profile called'
+" >> ~/.profile
+
+git clone git@github.com:StevenDRiggs/MyConfig.git
+cat ~/MyConfig/.bashrc >> ~/.bashrc
+cat ~/MyConfig/.bashrc >> ~/.bash_profile
+cat ~/MyConfig/.bashrc >> ~/.profile
+
 cp ~/MyConfig/.vimrc ~
-vim -c ':PluginInstall' -c 'q' -c ':%s/^"//' -c ':w' -c ':so %' -c ':PluginInstall' -c 'q' -c ':%s/^"//' -c ':wq' ~/.vimrc
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim -c ':PluginInstall' -c ':qa' ~/.vimrc
+vim -c ':%s/"//' -c ':w' -c ':source %' -c ':PluginInstall' -c ':qa' ~/.vimrc
+vim -c ':%s/"//' -c ':wq' ~/.vimrc
+
+rm -rf MyConfig/
+
+exec $SHELL
+
+read -p 'Ruby Version?: ' ruby_version
+rbenv install $ruby_version -v
+rbenv global $ruby_version
+
+gem install bundler rails rspec rspec-rails
+
+curl https://www.python.org/ftp/python/3.9.9/Python-3.9.9.tgz --output Python-3.9.9.tgz
+tar -xf ./Python-3.9.9.tgz
+cd Python-3.9.9.tgz
+./configure
+make altinstall
+
+cd ~
+
+pkg autoremove -y
