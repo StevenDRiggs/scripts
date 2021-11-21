18c64935cdfa240e0d1c49456aa969d95e94eb42
StevenDRiggs <sfriggs21@gmail.com>
2021-11-20 17:54:53 -0700

    refined and tested

--- ec2_ubuntu_20.04_setup.sh
+++ ec2_ubuntu_20.04_setup.sh
@@ -13,133 +13,107 @@ git config --global user.email $git_email
 git config --global submodule.recurse true
 
 # sudo vim .ssh/authorized_keys
-# 
-# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-# echo 'eval "$($HOME/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> $HOME/.profile
-# eval "$($HOME/linuxbrew/.linuxbrew/bin/brew shellenv)"
-# 
-# sudo apt install build-essential -y
-# 
-# sudo apt install apt-transport-https automake bison ca-certificates dirmngr g++ gcc gifsicle git-core gnupg jpegoptim libcurl4-openssl-dev libffi-dev libgdbm-dev libncurses5-dev libpq-dev libqt5webkit5-dev libreadline-dev libsqlite3-dev libssl-dev libtool libxml2-dev libxslt1-dev libyaml-dev make nodejs optipng qt5-default redis-server redis-tools ruby-dev software-properties-common sqlite3 tree yarn zlib1g-dev -y
-# # sudo apt install postgresql postgresql-contrib
-# 
-# brew install postgresql
-# 
-# sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
-# export NVM_DIR="$HOME/.nvm"
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-# nvm install node
-# npm install -g npm
-# npm install -g yarn
-# yarn global add typescript @types/jest
-# 
-# sudo add-apt-repository -y ppa:redislabs/redis
-# sudo apt update -y && sudo apt upgrade -y
-# sudo apt install redis -y
-# 
-# brew install rbenv -v
-# rbenv init
-# echo '
-# export PATH="$HOME/.rbenv/bin:$PATH"
-# eval "$(rbenv init -)"
-# export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> .bash_profile
-# curl -fsSL https://github.com/rbenv/rbenv-installer/raw/main/bin/rbenv-doctor | bash
-# 
-# cat ~/.bash_profile >> ~/.bashrc
-# cat ~/.profile >> ~/.bashrc
-# 
-# echo "
-# 
-# # If not running interactively, don't do anything
-# case \$- in
-#     *i*) ;;
-#       *) return;;
-# esac" >> ~/.bashrc
-# 
-# cp ~/.bashrc ~/.bash_profile
-# cp ~/.bashrc ~/.profile
-# 
-# echo "
-# 
-# echo '.bashrc called'
-# 
-# " >> ~/.bashrc
-# 
-# echo "
-# 
-# echo '.bash_profile called'
-# 
-# " >> ~/.bash_profile
-# 
-# echo "
-# 
-# echo '.profile called'
-# 
-# " >> ~/.profile
-# 
-# git clone git@github.com:StevenDRiggs/MyConfig.git
-# cat ~/MyConfig/.bashrc >> ~/.bashrc
-# cat ~/MyConfig/.bashrc >> ~/.bash_profile
-# cat ~/MyConfig/.bashrc >> ~/.profile
-# 
-# cp ~/MyConfig/.vimrc ~
-# git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-# vim -c ':PluginInstall' -c ':qa' ~/.vimrc
-# vim -c ':%s/"//' -c ':w' -c ':source %' -c ':PluginInstall' -c ':qa' ~/.vimrc
-# vim -c ':%s/"//' -c ':wq' ~/.vimrc
-# 
-# rm -rf MyConfig/
-# 
-# exec $SHELL
-# 
-# read -p 'Ruby Version?: ' ruby_version
-# rbenv install $ruby_version -v
-# rbenv global $ruby_version
-# 
-# gem install bundler rails rspec rspec-rails
-# 
-# curl https://www.python.org/ftp/python/3.9.9/Python-3.9.9.tgz --output Python-3.9.9.tgz
-# tar -xf ./Python-3.9.9.tgz
-# cd Python-3.9.9.tgz
-# ./configure
-# sudo make altinstall
-# 
-# cd ~
-# 
-# ## nginx
-# # sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 561F9B9CAC40B2F7
-# # sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger focal main > /etc/apt/sources.list.d/passenger.list'
-# # sudo apt update -y && sudo apt upgrade -y
-# # sudo apt install nginx-extras libnginx-mod-http-passenger -y
-# # if [ ! -f /etc/nginx/modules-enabled/50-mod-http-passenger.conf ]; then sudo ln -s /usr/share/nginx/modules-available/mod-http-passenger.load /etc/nginx/modules-enabled/50-mod-http-passenger.conf ; fi
-# # sudo ls /etc/nginx/conf.d/mod-http-passenger.conf
-# # 
-# # sudo vim -c ":%s/\/usr\/bin\/passenger_free_ruby/$(which ruby | sed 's|\/|\\\/|g')/" -c "s/\/\//\//g" -c ":wq" /etc/nginx/conf.d/mod-http-passenger.conf
-# # echo '
-# # server {
-# #   listen 80;
-# #   listen [::]:80;
-# # 
-# #   server_name stevendriggs.com;
-# #   root /home/deploy/aws_rails/current/public;
-# # 
-# #   passenger_enabled on;
-# #   passenger_app_env production;
-# # 
-# #   location /cable {
-# #     passenger_app_group_name aws_rails_websocket;
-# #     passenger_force_max_concurrent_requests_per_process 0;
-# #   }
-# # 
-# #   client_max_body_size 200m;
-# # 
-# #   location ~ ^/(assets|packs) {
-# #     expires max;
-# #     gzip_static on;
-# #   }
-# # }' | sudo tee /etc/nginx/sites-enabled/stevendriggs.com
-# # 
-# # sudo service nginx start
-# 
-# sudo apt autoremove -y
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> $HOME/.profile
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+
+sudo apt install build-essential -y
+
+sudo apt install apt-transport-https automake bison ca-certificates dirmngr g++ gcc gifsicle git-core gnupg jpegoptim libcurl4-openssl-dev libffi-dev libgdbm-dev libncurses5-dev libpq-dev libqt5webkit5-dev libreadline-dev libsqlite3-dev libssl-dev libtool libxml2-dev libxslt1-dev libyaml-dev make nodejs optipng qt5-default redis-server redis-tools ruby-dev software-properties-common sqlite3 tree yarn zlib1g-dev -y
+# sudo apt install postgresql postgresql-contrib
+
+brew install postgresql
+
+sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+nvm install node
+npm install -g npm
+npm install -g yarn
+yarn global add typescript @types/jest
+
+sudo add-apt-repository -y ppa:redislabs/redis
+sudo apt update -y && sudo apt upgrade -y
+sudo apt install redis -y
+
+brew install rbenv -v
+rbenv init
+echo '
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init - bash)"
+export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> .bash_profile
+curl -fsSL https://github.com/rbenv/rbenv-installer/raw/main/bin/rbenv-doctor | bash
+
+cat ~/.bash_profile >> ~/.bashrc
+cat ~/.profile >> ~/.bashrc
+
+echo "
+
+# If not running interactively, don't do anything
+case \$- in
+    *i*) ;;
+      *) return;;
+esac" >> ~/.bashrc
+
+cp ~/.bashrc ~/.bash_profile
+cp ~/.bashrc ~/.profile
+
+echo "
+
+echo '.bashrc called'
+
+" >> ~/.bashrc
+
+echo "
+
+echo '.bash_profile called'
+
+" >> ~/.bash_profile
+
+echo "
+
+echo '.profile called'
+
+" >> ~/.profile
+
+git clone https://github.com/StevenDRiggs/MyConfig.git
+cat ~/MyConfig/.bashrc >> ~/.bashrc
+cat ~/MyConfig/.bashrc >> ~/.bash_profile
+cat ~/MyConfig/.bashrc >> ~/.profile
+
+cp ~/MyConfig/.vimrc ~
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim -c ':PluginInstall' -c ':qa' ~/.vimrc
+vim -c ':%s/"//' -c ':w' -c ':source %' -c ':PluginInstall' -c ':qa' ~/.vimrc
+vim -c ':%s/"//' -c ':wq' ~/.vimrc
+
+rm -rf MyConfig/
+
+source ~/.bashrc
+
+read -p 'Ruby Version? (Enter for default 3.0.2): ' ruby_version
+if [[ ${#ruby_version} -eq 0 ]]; then
+	ruby_version=3.0.2
+fi
+rbenv install $ruby_version -v
+rbenv global $ruby_version
+
+gem install bundler rails rspec rspec-rails
+
+read -p 'Python Version? (Enter for default 3.9): ' python_version
+if [[ ${#python_version} -eq 0 ]]; then
+	python_version=3.9
+fi
+brew install python@$python_version
+
+cd ~
+
+pip3 install pip virtualenv
+
+sudo apt update -y && sudo apt upgrade -y
+
+sudo apt autoremove -y
+
+exec $SHELL
